cb41bee0fccf1c4b6aadfef3ab434cbcea1d8401
Italo Lon kan <sanchotorres777@gmail.com>
2023-02-09 12:16:36 -0500

    Update solver spec file

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,14 +1,21 @@
 require_relative '.././solver.rb'
 
 describe Solver do
+  solver = Solver
   context 'behavior.' do
+    it 'should return a object of factorial' do
+      expect(solver).instance_of? Solver
+    end
+
+    it 'should return 1 to factorial of 0.' do
+      expect(Solver.factorial(0)).to eql(1)
+    end
+
     it 'should return 6 to factorial of 3.' do
       expect(Solver.factorial(3)).to eql(6)
     end
 
-    it 'should return a factorial number of 5.'
+    it 'should return a factorial number of 5.' do
       expect(Solver.factorial(5)).to eql(120)
     end
   end
-
-end
