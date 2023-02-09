c7591cb41ce1c27fc648a9f6f4d1026174f533bf
Italo Lon kan <sanchotorres777@gmail.com>
2023-02-09 12:24:09 -0500

    Update solver file

--- solver.rb
+++ solver.rb
@@ -1,6 +1,20 @@
 class Solver
-  def self.factorial(n)
-    return 1 if n <= 1
-    factorial(n-1) * n
+  def self.factorial(num)
+    return 'Error' if num.instance_of?(String)
+    return 'Error' if num.negative?
+    return 1 if num <= 1
+    factorial(num - 1) * num
+  end
+  def self.fizzbuzz(num)
+    return 'fizzbuzz' if (num % 3).zero? && (num % 5).zero?
+    return 'fizz' if (num % 3).zero?
+    return 'buzz' if (num % 5).zero?
+
+    num.to_s
+  end
+
+  def self.reverse(str)
+    return -1 if str.instance_of(Numeric)
+    str.reverse
   end
 end
